30f689e90c8d9573135b01e916297ac77b716764
removedporn <86824510+removedporn@users.noreply.github.com>
2021-10-04 01:00:42 +0800

    Delete sbt.bat

--- sbt.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set SCRIPT_DIR=%~dp0
-java -Xms512M -Xmx1536M -Xss1M -XX:+CMSClassUnloadingEnabled -XX:MaxPermSize=256M %JAVA_OPTS% -Dfile.encoding=UTF-8 -jar "%SCRIPT_DIR%sbt-launch.jar" %*
